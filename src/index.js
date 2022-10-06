9a31e84942cb68377ba28aa4c989dbb663ae5f1b
kelseytoczek <kelseytoczek@gmail.com>
2022-10-06 12:13:03 -0400

    fix https

--- src/index.js
+++ src/index.js
@@ -109,7 +109,7 @@ function displayWeatherCondition(response) {
 
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
